a7f3b27062baf0e72d1b05b87b22883e204fbd7c
pnayak770 <pintunayak770@gmail.com>
2024-12-14 23:11:09 +0530

    done

--- day18-MVCproject/route/product.route.js
+++ day18-MVCproject/route/product.route.js
@@ -1,16 +1,13 @@
 import express from "express";
 
+
 const router=express.Router();
 
 router.get("/product/list",)
 
 router.get("/product-review",)
 
-router.get("/product-rate",(req,res)=>{
-    res.json({
-        succes:true,
-    })
-})
+router.get("/product-rate",)
 
 
  export default router
